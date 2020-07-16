ff08ee57967e72a40e153bab3577c2e4bdbfa080
Annyce <annyce.davis@gmail.com>
2020-07-16 13:48:47 -0400

    [+] Used string resources

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,5 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.textView.text = getString(R.string.middle_text)
     }
 }
