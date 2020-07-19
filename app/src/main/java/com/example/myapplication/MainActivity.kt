5af8614168be46d5101fab5d0553c3e99711a029
Annyce <annyce.davis@gmail.com>
2020-07-19 17:41:47 -0400

    [+] Displayed a Snackbar with an action

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,9 +21,15 @@ class MainActivity : AppCompatActivity() {
             val password = binding.password.text.toString()
             val message = getString(R.string.message_text, email, password)
 
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            Snackbar.make(it, "I'm a Snackbar! 👋", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Show info") { showMessage(message) }
+                .show()
 
             Log.i("MainActivity", message)
         }
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
