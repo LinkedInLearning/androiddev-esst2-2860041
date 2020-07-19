4fcb250e622e0303805db82394b45e1144ea22bf
Annyce <annyce.davis@gmail.com>
2020-07-19 11:02:34 -0400

    [+] Empty project with view binding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,7 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.textView.text = getString(R.string.middle_text)
     }
 }
