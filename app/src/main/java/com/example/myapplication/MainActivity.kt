45bcb6282f945b2559a8046c96e59077fec14fa1
Annyce <annyce.davis@gmail.com>
2020-08-01 08:17:29 -0400

    [+] Added an images folder
    * Upgraded the Koltin and Android Gradle plugins

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,15 +13,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.button.setOnClickListener {
-            val firstName = binding.firstNameText.text.toString()
-            val favoriteColor = binding.favoriteColorText.text.toString()
-            val favoriteSnack = binding.favoriteSnackText.text.toString()
-            val message = "Hi $firstName, your favorite color is $favoriteColor " +
-                    "and your favorite snack is $favoriteSnack"
-
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-        }
     }
 }
